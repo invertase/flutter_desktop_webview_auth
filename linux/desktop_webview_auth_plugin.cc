671709d19cf7e3f2cfefbcbcc59f1b58aea72719
pr-mais <hiimechi@gmail.com>
2022-02-03 13:40:54 +0300

    fix: method channel on linux

--- linux/desktop_webview_auth_plugin.cc
+++ linux/desktop_webview_auth_plugin.cc
@@ -11,53 +11,40 @@
   (G_TYPE_CHECK_INSTANCE_CAST((obj), desktop_webview_auth_plugin_get_type(), \
                               DesktopWebviewAuthPlugin))
 
+DesktopWebviewAuthPlugin *plugin;
+
 struct _DesktopWebviewAuthPlugin
 {
   GObject parent_instance;
-  FlPluginRegistrar* registrar;
-  FlMethodChannel* method_channel;
+  FlPluginRegistrar *registrar;
+  FlMethodChannel *method_channel;
   gchar *redirectUri;
-  gchar *callbackUri;
   GtkWidget *popupWindow;
-} plugin;
+};
 
 G_DEFINE_TYPE(DesktopWebviewAuthPlugin, desktop_webview_auth_plugin, g_object_get_type())
 
-static void changed (WebKitWebView *view, WebKitLoadEvent event, gpointer user_data)
+static void changed(WebKitWebView *view, WebKitLoadEvent event, gpointer user_data)
 {
-  DesktopWebviewAuthPlugin* plugin = DESKTOP_WEBVIEW_AUTH_PLUGIN(user_data);
-
-  const gchar *uri = webkit_web_view_get_uri (view);
+  const gchar *uri = webkit_web_view_get_uri(view);
   const gchar *redirectUri = plugin->redirectUri;
 
-  char str[(int) strlen(redirectUri)];
-  const gchar *copy = strncpy(str, (char*) uri, (int) strlen(redirectUri));
+  char str[(int)strlen(redirectUri)];
+  const gchar *copy = strncpy(str, (char *)uri, (int)strlen(redirectUri));
 
-  bool matching = strcmp(copy, redirectUri);
+  bool matching = strcmp(copy, redirectUri) == 0;
 
-  g_print("%s \n", copy);
-
-  g_print (
-    "======================= redirectUri ======================= \n length:%d content: %s matching? %s\n",
-    (int) strlen(redirectUri),
-    redirectUri,
-    matching == 1? "False" : "True"
-  );
-  
-  if(event == WEBKIT_LOAD_REDIRECTED && matching) {
-    g_free (plugin->redirectUri);
-
-    g_print("callback url: %s\n", webkit_web_view_get_uri (view));
-    const gchar *callbackUri = (gchar*) webkit_web_view_get_uri (view);
-
+  if (event == WEBKIT_LOAD_FINISHED && matching)
+  {
+    const gchar *callbackUri = (gchar *)webkit_web_view_get_uri(view);
     g_autoptr(FlValue) result = fl_value_new_string(callbackUri);
 
-    //response = FL_METHOD_RESPONSE(fl_method_success_response_new(value));
-
     fl_method_channel_invoke_method(plugin->method_channel, "callbackUri",
-                                    result, nullptr, nullptr, plugin);
+                                    result, nullptr, nullptr, &plugin);
 
-    gtk_window_close (GTK_WINDOW(plugin->popupWindow));
+    g_free(plugin->redirectUri);
+
+    gtk_window_close(GTK_WINDOW(plugin->popupWindow));
   }
 }
 
@@ -71,7 +58,7 @@ static void desktop_webview_auth_plugin_handle_method_call(
   FlValue *args = fl_method_call_get_args(method_call);
 
   if (strcmp(method, "signIn") == 0)
-  {    
+  {
     GtkWidget *popup = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_transient_for(GTK_WINDOW(popup), GTK_WINDOW(window));
 
@@ -91,17 +78,21 @@ static void desktop_webview_auth_plugin_handle_method_call(
     gtk_window_set_position(GTK_WINDOW(popup), GTK_WIN_POS_MOUSE);
     gtk_window_set_default_size(GTK_WINDOW(popup), 980, 720);
 
-    plugin.redirectUri = g_strdup (redirectUri);
-    plugin.popupWindow = popup;
+    plugin->redirectUri = g_strdup(redirectUri);
+    plugin->popupWindow = popup;
 
-    g_signal_connect(G_OBJECT (webview), "load-changed", G_CALLBACK (changed), &plugin);
+    g_signal_connect(G_OBJECT(webview), "load-changed", G_CALLBACK(changed), NULL);
 
     gtk_widget_show_all(popup);
+
+    response = FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
   }
   else
   {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
+
+  fl_method_call_respond(method_call, response, nullptr);
 }
 
 static void desktop_webview_auth_plugin_dispose(GObject *object)
@@ -125,18 +116,20 @@ static void method_call_cb(FlMethodChannel *channel, FlMethodCall *method_call,
 
 void desktop_webview_auth_plugin_register_with_registrar(FlPluginRegistrar *registrar)
 {
-  DesktopWebviewAuthPlugin *plugin = DESKTOP_WEBVIEW_AUTH_PLUGIN(
+  DesktopWebviewAuthPlugin *self = DESKTOP_WEBVIEW_AUTH_PLUGIN(
       g_object_new(desktop_webview_auth_plugin_get_type(), nullptr));
 
   g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
-  g_autoptr(FlMethodChannel) method_channel =
+
+  self->method_channel =
       fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
                             "io.invertase.flutter/desktop_webview_auth",
                             FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(method_channel, method_call_cb,
-                                            g_object_ref(plugin),
+
+  fl_method_channel_set_method_call_handler(self->method_channel, method_call_cb,
+                                            g_object_ref(self),
                                             g_object_unref);
 
-  plugin->method_channel = g_object_ref(method_channel);
-
+  plugin = self;
+  g_object_unref(plugin);
 }
