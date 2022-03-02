8f510b61af9d43e83575a8b96ea8bf5cfd5c7a41
pr-mais <hiimechi@gmail.com>
2022-03-02 20:57:07 +0300

    feat: popup window with webview

--- windows/desktop_webview_auth_plugin.cpp
+++ windows/desktop_webview_auth_plugin.cpp
@@ -2,6 +2,9 @@
 
 // This must be included before many other Windows headers.
 #include <windows.h>
+#include <stdlib.h>
+#include <wrl.h>
+#include <wil/com.h>
 
 // For getPlatformVersion; remove unless needed for your plugin implementation.
 #include <VersionHelpers.h>
@@ -13,8 +16,17 @@
 #include <map>
 #include <memory>
 #include <sstream>
+#include <iostream>
 
-#include <WebView2.h>
+#include "WebView2.h"
+
+using namespace Microsoft::WRL;
+
+// Pointer to WebViewController
+static wil::com_ptr<ICoreWebView2Controller> webviewController;
+
+// Pointer to WebView window
+static wil::com_ptr<ICoreWebView2> webviewWindow;
 
 namespace {
 
@@ -70,25 +82,67 @@ void DesktopWebviewAuthPlugin::HandleMethodCall(
     const flutter::MethodCall<flutter::EncodableValue> &method_call,
     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
     if (method_call.method_name().compare("signIn") == 0) {
-      //HWND parentWindow = view_->GetNativeWindow();
-      HWND hwnd = CreateWindowEx(
-          0,                              // Optional window styles.
-          L"WebView Window",                     // Window class
-          L"Learn to Program Windows",    // Window text
-          WS_OVERLAPPEDWINDOW,            // Window style
 
+      RECT rect;
+      GetWindowRect(view_->GetNativeWindow(), &rect);
+
+      HWND hWnd = CreateWindowExA(
+          WS_EX_OVERLAPPEDWINDOW,
+          "WebView Window",
+          "",
+          WS_OVERLAPPEDWINDOW,
           // Size and position
-          CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-          view_->GetNativeWindow(),       // Parent window    
-          NULL,       // Menu
-          wc.hInstance,  // Instance handle
-          NULL        // Additional application data
+          rect.top, rect.top, 980, 720,
+          view_->GetNativeWindow(),
+          nullptr,
+          wc.hInstance,
+          nullptr
       );
 
-      ShowWindow(hwnd, 1);
+      ShowWindow(hWnd, 1);
 
-    result->Success(flutter::EncodableValue());
+      // Step 3 - Create a single WebView within the parent window
+      // Locate the browser and set up the environment for WebView
+      CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+        Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+            [hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+
+                  // Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+                  env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+                    [hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+                    if (controller != nullptr) {
+                        webviewController = controller;
+                        webviewController->get_CoreWebView2(&webviewWindow);
+                    }
+
+                    // Add a few settings for the webview
+                    // The demo step is redundant since the values are the default settings
+                    ICoreWebView2Settings* Settings;
+                    webviewWindow->get_Settings(&Settings);
+                    Settings->put_IsScriptEnabled(TRUE);
+                    Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+                    Settings->put_IsWebMessageEnabled(TRUE);
+
+                    // Resize the WebView2 control to fit the bounds of the parent window
+                    RECT bounds;
+                    GetClientRect(hWnd, &bounds);
+                    webviewController->put_Bounds(bounds);
+
+                    // Schedule an async task to navigate to Bing
+                    webviewWindow->Navigate(L"https://www.google.com/");
+
+                    // 4 - Navigation events
+
+                    // 5 - Scripting
+
+                    // 6 - Communication between host and web content
+
+                    return S_OK;
+                  }).Get());
+            return S_OK;
+        }).Get());
+
+      result->Success(flutter::EncodableValue());
   } else if (method_call.method_name().compare("recaptchaVerification") == 0) {
     //result->Success(flutter::EncodableValue(version_stream.str()));
     result->NotImplemented();
